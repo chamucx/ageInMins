8c538304773c6d7a19ff3f8b4da1056377f295c9
chamucx <joseantoniosysnet@gmail.com>
2022-09-03 13:47:36 +0200

    complted age in mins

--- app/src/main/java/com/example/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -8,17 +8,21 @@ import android.widget.Button
 import android.widget.DatePicker
 import android.widget.TextView
 import android.widget.Toast
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    val tvSelectedDate: TextView? = null
+    private var tvSelectedDate: TextView? = null
+    private var tvSelectedDateInMinutes: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val btnDatePicker: Button = findViewById(R.id.btnDatePicker)
+        tvSelectedDate = findViewById(R.id.tvSelectedDate)
+        tvSelectedDateInMinutes = findViewById(R.id.tvSelectedDateInMinutes)
 
         btnDatePicker.setOnClickListener {
 
@@ -32,22 +36,37 @@ class MainActivity : AppCompatActivity() {
         val year = myCalendar.get(Calendar.YEAR)
         val month = myCalendar.get(Calendar.MONTH)
         val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+        val presentDate =
+            DatePickerDialog(
+                this,
+                { view, selectedYear, selectedMonth, dayOfMonth ->
+                    Toast.makeText(
+                        this,
+                        "Year was $selectedYear, month ${selectedMonth + 1}, day of month $dayOfMonth",
+                        Toast.LENGTH_LONG
+                    ).show()
 
+                    val selectedDate = "$dayOfMonth/${selectedMonth + 1}/${selectedYear}"
+                    tvSelectedDate?.text = selectedDate
 
-        DatePickerDialog(
-            this,
-            { view, selectedYear, selectedMonth, dayOfMonth ->
-                Toast.makeText(
-                    this,
-                    "Year was $selectedYear, month ${selectedMonth + 1}, day of month $dayOfMonth",
-                    Toast.LENGTH_LONG
-                )
-                    .show()
+                    val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+                    val theDate = sdf.parse(selectedDate)
+                    theDate?.let {
+                        val selectedDateInMins = theDate.time / 60000
 
-            },
-            year, month, day
-        ).show()
+                        val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
 
+                        val currentDateInMinutes = currentDate.time / 60000
+
+                        val differenceInMins = currentDateInMinutes - selectedDateInMins
+                        tvSelectedDateInMinutes?.text = differenceInMins.toString()
+                    }
+                },
+                year, month, day
+            )
+
+        presentDate.datePicker.maxDate = System.currentTimeMillis() - 86400000
+        presentDate.show()
 
     }
 }
